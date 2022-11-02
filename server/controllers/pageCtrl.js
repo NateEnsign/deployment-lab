ed1d99faac58eb5648317b24134d58b11857c1af
Nate Ensign <dixienate@gmail.com>
2022-11-02 15:34:05 -0600

    maybe complete

--- server/controllers/pageCtrl.js
+++ server/controllers/pageCtrl.js
@@ -4,5 +4,9 @@ module.exports = {
 
     home: (req, res) => {
         res.sendFile(path.join(__dirname, "../../index.html"))
-    }
+    },
+
+    app.get('/style', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/index.html'))
+      })
 }
